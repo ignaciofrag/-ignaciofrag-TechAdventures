b99518d44681e94a83e06e0919915f0d8173cfa7
Ignacio Garfias Aguirre <ignaciofrag@gmail.com>
2024-03-30 00:24:15 +0100

    integrando costes.js

---
+++ costes.js
@@ -0,0 +1,7 @@
+// Definición de costos
+let costo_solicitud_carta = 75.05;
+let costo_expedicion_por_invitado = 6.54;
+
+// Cálculo para 5 invitados
+let costo_total_5_invitados = costo_solicitud_carta + (costo_expedicion_por_invitado * 5);
+console.log(costo_total_5_invitados);
