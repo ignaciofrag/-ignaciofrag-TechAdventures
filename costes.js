e721fa9f25501705c28ba8e1c7b1cbfe5efa4357
Ignacio Garfias Aguirre <ignaciofrag@gmail.com>
2024-03-30 01:47:16 +0100

    Agrega .html para costes.js

--- costes.js
+++ costes.js
@@ -1,20 +1,8 @@
-// Definición de costos
 let costo_solicitud_carta = 75.05;
 let costo_expedicion_por_invitado = 6.54;
 
-
-// Función para calcular el costo total basado en el número de invitados
+// Función global para calcular el costo total
 function calcularCostoTotal(numInvitados) {
   let costoTotal = costo_solicitud_carta + (costo_expedicion_por_invitado * numInvitados);
-  return costoTotal;
+  return costoTotal.toFixed(2); // Asegura que el resultado tenga dos decimales
 }
-
-// Calculando y mostrando el costo para 1, 2, 3, y 4 invitados
-console.log(`Costo para 1 invitado: ${calcularCostoTotal(1)}€`);
-console.log(`Costo para 2 invitados: ${calcularCostoTotal(2)}€`);
-console.log(`Costo para 3 invitados: ${calcularCostoTotal(3)}€`);
-console.log(`Costo para 4 invitados: ${calcularCostoTotal(4)}€`);
-
-// Permitir al usuario introducir un número de invitados
-let numeroDeInvitados = prompt("Introduce el número de invitados:");
-console.log(`Costo para ${numeroDeInvitados} invitados: ${calcularCostoTotal(numeroDeInvitados)}€`);
